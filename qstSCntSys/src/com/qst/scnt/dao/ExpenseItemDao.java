483ae0467ab498e8b8e305f202a3cc47e3f02a92
RaintoSunSoHappy <1243553274@qq.com>
2018-07-13 22:31:54 +0800

    费用管理dao,service

--- qstSCntSys/src/com/qst/scnt/dao/ExpenseItemDao.java
+++ qstSCntSys/src/com/qst/scnt/dao/ExpenseItemDao.java
@@ -1,7 +1,11 @@
 package com.qst.scnt.dao;
 
+import java.util.List;
+
 import com.qst.scnt.model.ExpenseItem;
 
 public interface ExpenseItemDao   extends BaseDao<ExpenseItem> {
+	
+	public List<ExpenseItem> selectAll();
 
 }
